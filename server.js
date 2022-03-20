0a49fa85939e7d7e6c1bd0798eb7974ec95e8455
Rishikesh Pimpale <rishikesh_pimpale@redmane.com>
2022-03-20 17:56:51 -0500

    Add socket.io for two way communication

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// socket.io needs to tie in with Express
+// create a regular HTTP server with Node that will then share with express and socket.io
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 app.use(express.static(__dirname));
 
 app.use(bodyParser.json());
@@ -22,9 +27,14 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     messages.push(req.body);
+    io.emit('message', req.body);
     res.sendStatus(200);
 });
 
-const server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+const server = http.listen(3000, () => {
     console.log('Server started on port', server.address().port);
 });
