c9ac27e2fc3267a5ff6beb4b192611c1c3ef143f
Rishikesh Pimpale <rishikesh_pimpale@redmane.com>
2022-03-20 17:17:27 -0500

    Add get and post endpoints

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(express.static(__dirname));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let messages = [
+    {
+        name: 'Admin',
+        message: 'Welcome to the chatroom!'
+    }
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
+});
 
 const server = app.listen(3000, () => {
     console.log('Server started on port', server.address().port);
