1329f08707453d086811d86f3e738031eb2d49ef
Rishikesh Pimpale <rishikesh_pimpale@redmane.com>
2022-03-23 08:05:02 -0500

    Util function to prevent XSS

---
+++ util.js
@@ -0,0 +1,9 @@
+// sanitize data to prevent XSS
+const sanitize = (text) => {
+    return text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
